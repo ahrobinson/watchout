b5bee6fe6e3754963ccae109262f6cc38509b07f
ahrobinson <ahrobins3@gmail.com>
2015-10-16 22:01:22 -0500

    asteroids are moving!

--- client/watchout.js
+++ client/watchout.js
@@ -63,15 +63,15 @@ var axes = {
   y: d3.scale.linear().domain([0,100]).range([0,gameOptions.height])
 };
 
-var gameBoard = d3.select(".container")
-                .append("svg:svg")
+var gameBoard = d3.select('.container')
+                .append('svg:svg')
                 .attr('width', gameOptions.width)
                 .attr('height', gameOptions.height);
 
 //Update score function
 var updateScore = function() {
   d3.select('.current')
-    .text(gameStats.score.toString())
+    .text(gameStats.score.toString());
 };
 
 //Update best score
@@ -98,12 +98,54 @@ var createEnemies = function() {
   //Returns and array of enemy objects, with the properties "x" and "y"
   var enemiesArray = range(0, gameOptions.nEnemies).map(function(i){
     return {id: i, x: Math.random()*100, y: Math.random()*100}
-  })
+  });
 
   return enemiesArray;
 };
 
+//Render the game board
+var render = function(enemy_data) {
+  //Select all the enemies on the board and bind the data to them, using the enemies' id attribute as a key to ensure we update enemies in the future  
+  var enemies = gameBoard.selectAll('svg').data(enemy_data, function(d) {
+    return d.id;
+  });
+
+  enemies.enter().append('image')
+    .attr('xlink:href','asteroid.png')
+    .attr('x', function(enemy) { return axes.x(enemy.x) })
+    .attr('y', function(enemy) { return axes.y(enemy.y) })
+    .attr('width', 25)
+    .attr('height', 25)
+    .attr('class', 'asteroid')
+
+};
+
+//Transition
+//# transition.attrTween(name, tween)
+//https://github.com/mbostock/d3/wiki/Transitions#attrTween
+//Use setInterval
+
+//Selects all the asteroid and puts them on one point - select individual asteroid, then make sure they go to random points
+// d3.selectAll('.asteroid').transition().attr('x', Math.random() * 700).attr('y', Math.random() * 450)
+
+//select individual asteroids and then call the below function
+
+var moveEnemy = function() {
+  
+  d3.selectAll('.asteroid').each(function() {
+    d3.select(this).transition().attr('x', Math.random() * 700).attr('y', Math.random() * 450)
+  })
+  
+}
+
+setInterval(function() {moveEnemy()}, 1000);
 
 
+
+
+
+
+var newEnemies = createEnemies();
+render(newEnemies);
 
 
